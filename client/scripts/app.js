4abe7efb8214b07af966c1b5449c233038ae3813
Hack Reactor Student <student@hackreactor.com>
2017-05-26 15:33:48 -0700

    implement sending fetching and chatroom behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,39 @@
-let app = {};
+let app = {
+  init: function() {    
+    app.server = 'http://parse.sfm8.hackreactor.com';    
+  },
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+  renderMessage: function(message) {
+    $('#chats').append(`<div>${message}</div>`);    
+  },
+  renderRoom: function(room) {
+    $('#roomSelect').append(`<option value="${room}">${room}</option>`);
+  },
+  send: function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function() {
+    var data, success, dataType;
+    $.ajax({
+      url: app.server,
+      data: data,
+      success: success,
+      dataType: dataType
+    });
+  }
+};
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -88,7 +88,6 @@ describe('chatterbox', function() {
 
       it('should be able to add rooms to the DOM', function() {
         app.renderRoom('superLobby');
-
         expect($('#roomSelect').children().length).to.equal(1);
       });
 
