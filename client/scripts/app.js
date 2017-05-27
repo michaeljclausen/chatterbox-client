58c469aa63634453b43c3dd2c3dbaceaeefe8a18
Hack Reactor Student <student@hackreactor.com>
2017-05-26 22:42:04 -0700

    implement room select and display messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,34 @@
-let app = {
-  init: function() {    
-    app.server = 'http://parse.sfm8.hackreactor.com';    
-  },
-  clearMessages: function() {
+class App {
+  constructor() {
+    this.init();
+  }
+  init() { 
+    this.fetch();
+  }
+  clearMessages() {
     $('#chats').empty();
-  },
-  renderMessage: function(message) {
-    $('#chats').append(`<div>${message}</div>`);    
-  },
-  renderRoom: function(room) {
-    $('#roomSelect').append(`<option value="${room}">${room}</option>`);
-  },
-  send: function(message) {
+  }
+  renderMessage(message) {
+    //console.log(message);
+    $('#chats').append(`<div class='username'>${message.username}:</div>`);
+    $('#chats').append(`<div class='text'>${message.text}</div>`);
+  }
+  renderRoom(room) {
+    this.clearMessages();
+    this.fetch();
+  }
+  sendMessage(username, roomname, text) {
+    //console.log(`${username}, ${roomname}, ${text} `);
+    let message = {
+      username: username,
+      text: text,
+      roomname: roomname
+    };
+    console.log(message);
+    this.send(message);
+    this.renderRoom();
+  }
+  send(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
@@ -26,14 +43,48 @@ let app = {
         console.error('chatterbox: Failed to send message', data);
       }
     });
-  },
-  fetch: function() {
-    var data, success, dataType;
+  }
+  fetch() {
     $.ajax({
-      url: app.server,
-      data: data,
-      success: success,
-      dataType: dataType
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      data: this.messages,
+      contentType: 'application/json',
+      success: function (data) {
+        let messages = data.results;
+        let rooms = {};
+        let selectedRoom = $('#roomSelect').find(':selected').text();
+        if (!selectedRoom) {
+          messages.forEach(message => {
+            if (message.hasOwnProperty('roomname')) {
+              rooms[message['roomname']] = 1;
+            }
+          });
+          
+          for (let room in rooms) {
+            $('#roomSelect').append($('<option>', {
+              value: room,
+              text: room
+            }));
+          }
+        }
+        
+        
+        //console.log(selectedRoom);
+        
+        messages.forEach(message => {
+          if (message.roomname === selectedRoom) {
+            this.renderMessage(message); 
+          } 
+        });
+      }.bind(this),
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
     });
   }
-};
+}
+
+let app = new App();
